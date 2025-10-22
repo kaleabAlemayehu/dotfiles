cac5dfbb79fb43265f6248983cd5389eeedf8eee
Kaleab Alemayehu ( Neo ) <cipherlofi04@gmail.com>
2025-10-22 10:28:28 -0400

    feat: fixing lazynvim error and adding bash_profile so that tmux can read source bashrc when it starts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,11 +6,11 @@ require 'core.health'
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=sable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
@@ -39,9 +39,9 @@ require('lazy').setup({
   require 'plugins.lazygit',
   require 'plugins.flash',
   -- require 'plugins.copilot',
-  require 'plugins.windsurf',
+  -- require 'plugins.windsurf',
   require 'plugins.blame',
-  require 'plugins.supermaven',
+  -- require 'plugins.supermaven',
   -- require 'plugins.autosession',
   require 'theme.material',
 }, {
